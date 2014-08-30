ae087e8b6039bb4ee980aa8916a5ec8919023bfb
magengbin <magengbin@msn.com>
2014-08-31 01:02:55 +0800

    modiffied test.cpp show run total time

--- sort/merge_sort/test.cpp
+++ sort/merge_sort/test.cpp
@@ -1,17 +1,30 @@
 #include "merge_sort.h"
 #include <iostream>
-
-int main()
+#include <ctime>
+#include <cstdlib>
+int main(int argc, char **argv)
 {
-    const int number = 100000;
+    
+    int number = 100000;
+    if (argc == 2)
+    {
+        int number = atoi(argv[1]);
+    }
+    
     int *A = new int[number];
+    if (A == NULL)
+    {
+        std::cerr << "not full memory" << std::endl;
+    }
+    
     for (int i = 0; i < number; i++)
     {
         std::cin >> A[i];
     }
-
+    clock_t start = clock();
     merge_sort(A, number);
-
+    clock_t finish = clock();
+    std::cout << static_cast<double>(finish-start)/CLOCKS_PER_SEC << " s" << std::endl;
     for (int i = 0; i < number; i++)
     {
         std::cout << A[i] << std::endl;
